621058ad3ff2a865cbe0b60a236e3d79e12c8ed1
Bhuvan9933 <2303031241359@paruluniversity.ac.in>
2025-03-06 21:10:27 +0530

    Update app.py
    Signed-off-by: Bhuvan9933 <2303031241359@paruluniversity.ac.in>

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key"  # Replace this with a secure key
+app.secret_key = "6i0zseuDb37drmsBMyoZ4CGRL2TXXg97LoePCRIH"  # Replace this with a secure key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
